5415987534c9a9155a7d5ad0f23e99dd75f03850
bella-sheridan <isabellasheridan1@gmail.com>
2024-02-10 11:57:29 -0600

    shamuel silly class

--- ActualCode/NewSwerveTest-DO_NOT_LOOK/src/main/cpp/TurnMotor.cpp
+++ ActualCode/NewSwerveTest-DO_NOT_LOOK/src/main/cpp/TurnMotor.cpp
@@ -1,3 +1,20 @@
 #include <fmt/core.h>
 #include <frc/smartdashboard/SmartDashboard.h>
+// #include 
 
+class TurnMotor
+{
+private:
+    /* data */
+public:
+    TurnMotor(/* args */);
+    ~TurnMotor();
+};
+
+TurnMotor::TurnMotor(/* args */)
+{
+}
+
+TurnMotor::~TurnMotor()
+{
+}
